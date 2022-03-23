2bd1682c63427ef7f6b76fe9002cf4ed71db72a3
interesik <interesik.pro@gmail.com>
2022-03-23 15:20:02 +0100

    Zadanie 0

---
+++ Mathexample/Mathex.cs
@@ -0,0 +1,46 @@
+﻿namespace Mathexample
+{
+    public static class Mathex
+    {
+        public static int add(int a,int b) 
+        { 
+            return a + b;
+        }
+        public static double add(double a, double b)
+        {
+            return a + b;
+        }
+        public static int subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static double subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public static dynamic div(int a, int b) 
+        {
+            try { return a / b; }
+            catch (DivideByZeroException) { throw new DivideByZeroException(); }
+        }
+        public static dynamic div(double a, double b)
+        {
+            try { return a / b; }
+            catch (DivideByZeroException) { throw new DivideByZeroException(); }
+        }
+        public static int mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double mul(double a,double b)
+        {
+            return a * b;
+        }
+
+        public static int mod(int a, int b) 
+        { 
+            return a % b; 
+        }
+    }
+}
